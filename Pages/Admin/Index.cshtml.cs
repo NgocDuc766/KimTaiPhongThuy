d418508b40354dc27a8a78f82072b5329866928f
hungnphe173547 <hungnphe173547@fpt.edu.vn>
2025-03-17 23:07:35 +0700

    upup

--- Pages/Admin/Index.cshtml.cs
+++ Pages/Admin/Index.cshtml.cs
@@ -1,12 +1,28 @@
+using KimTaiPhongThuy.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace KimTaiPhongThuy.Pages.Admin
 {
     public class IndexModel : PageModel
     {
+        private readonly JewelryStoreContext _context;
+
+        public IndexModel(JewelryStoreContext context)
+        {
+            _context = context;
+        }
+
+        public int TotalOrders { get; set; }
+        public int TotalProducts { get; set; }
+        public int TotalUsers { get; set; }
+
         public void OnGet()
         {
+            TotalOrders = _context.Orders.Count();
+            TotalProducts = _context.Products.Count();
+            TotalUsers = _context.Users.Count();
         }
     }
 }
